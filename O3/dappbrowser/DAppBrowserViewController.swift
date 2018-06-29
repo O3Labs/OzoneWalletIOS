91c45c9abe1caac32246b250627fe9034b7a7e86
Apisit Toompakdee <apmotive@gmail.com>
2018-06-29 16:27:12 +0900

    fixed language conflicts

--- O3/dappbrowser/DAppBrowserViewController.swift
+++ O3/dappbrowser/DAppBrowserViewController.swift
@@ -9,27 +9,40 @@
 import UIKit
 import WebKit
 
+extension Bundle {
+    var releaseVersionNumber: String? {
+        return infoDictionary?["CFBundleShortVersionString"] as? String
+    }
+    var buildVersionNumber: String? {
+        return infoDictionary?["CFBundleVersion"] as? String
+    }
+}
+
 class DAppBrowserViewController: UIViewController {
 
     @IBOutlet var containerView: UIView? = nil
     var webView: WKWebView?
-
+    var callbackMethodName: String = "callback"
+    let availableMethods = ["isAvailableHandler", "platformHandler", "getAccountsHandler", "getPublicKeyHandler", "initHandler", "connectHandler", "sendMessageHandler"]
+    
     override func loadView() {
         super.loadView()
         
+        //This is a list of available methods that web page can call O3 app
         let contentController = WKUserContentController()
-        contentController.add(self, name: "callbackHandler")
-        contentController.add(self, name: "o3Address")
+        for method in availableMethods {
+            contentController.add(self, name: method)
+        }
+    
         let config = WKWebViewConfiguration()
         config.userContentController = contentController
-        
         self.webView = WKWebView( frame: self.containerView!.bounds, configuration: config)
         self.view = self.webView
-        
     }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        let url = URL(string: "http://localhost:8000/")
+        let url = URL(string: "http://localhost:8000/example/")
         let req = URLRequest(url: url!)
         self.webView!.load(req)
         self.webView?.navigationDelegate = self
@@ -40,22 +53,20 @@ class DAppBrowserViewController: UIViewController {
         super.didReceiveMemoryWarning()
     }
     
-    
     @objc func refresh(_ sender: Any) {
         self.webView?.reload()
     }
     
-    func getLoggedInAddress(){
-        let dict = [
-            "address": Authenticated.account?.address
+    func callback(event: String, dictionary: [String: Any]) {
+        let dic: [String: Any] = [
+        "event":event,
+        "data":dictionary
         ]
-        let jsonData = try! JSONSerialization.data(withJSONObject: dict, options: [])
+        let jsonData = try! JSONSerialization.data(withJSONObject: dic, options: [])
         let jsonString = String(data: jsonData, encoding: String.Encoding.utf8)!
-        
         // Send the location update to the page
-        self.webView?.evaluateJavaScript("o3(\(jsonString))") { result, error in
+        self.webView?.evaluateJavaScript("o3.callback(\(jsonString))") { result, error in
             guard error == nil else {
-                print(error)
                 return
             }
         }
@@ -67,17 +78,44 @@ extension DAppBrowserViewController: WKScriptMessageHandler {
     
     
     func userContentController(_ userContentController: WKUserContentController, didReceive message: WKScriptMessage) {
-        if (message.name == "callbackHandler"){
-            print("\(message.body)")
-            let messageString = message.body
-            DispatchQueue.main.async {
-                OzoneAlert.alertDialog(message: messageString as! String, dismissTitle: "OK", didDismiss: {})
+//        if !availableMethods.contains(message.name) {
+//            return
+//        }
+        if (message.name == "sendMessageHandler"){
+            let jsonData = message.body as! [String: String]
+//            let data = messageString.data(using: .utf8)
+//
+//            let jsonData = try! JSONSerialization.jsonObject(with: data!, options: []) as! [String: String]
+            
+            if jsonData["command"] == "init" {
+                self.callback(event: "init", dictionary: [:])
+            } else if jsonData["command"] == "platform" {
+                self.callback(event: "platform", dictionary: ["platform": "ios", "version": Bundle.main.releaseVersionNumber])
             }
-        } else if (message.name == "o3Address"){
-            DispatchQueue.main.async {
-                self.getLoggedInAddress()
-            }
+            
+           
         }
+        if (message.name == "initHandler"){
+            self.callbackMethodName = message.body as! String
+        }else  if (message.name == "connectHandler"){
+            let appName = message.body as! String
+            let message = String(format:"%@ want to connect to your O3 app. Allow?", appName)
+            OzoneAlert.confirmDialog(message: message, cancelTitle: "Cancel", confirmTitle: "Allow", didCancel: {
+                
+            }) {
+                self.callback(event: "connect", dictionary: ["address": Authenticated.account!.address])
+            }
+        } else if (message.name == "platformHandler"){
+            self.callback(event: "platform", dictionary: ["platform": "ios", "version": Bundle.main.releaseVersionNumber])
+        } else if (message.name == "getAccountsHandler"){
+            let account = ["address": Authenticated.account!.address]
+            let dic = ["accounts": [account]]
+            self.callback(event: "accounts", dictionary: dic)
+        } else if (message.name == "isAvailableHandler"){
+            let dic = ["available": true]
+            self.callback(event: "is_o3_available", dictionary: dic)
+        }
+        
     }
     
 }
@@ -85,6 +123,6 @@ extension DAppBrowserViewController: WKScriptMessageHandler {
 extension DAppBrowserViewController: WKNavigationDelegate {
     
     func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        self.getLoggedInAddress()
+     
     }
 }
